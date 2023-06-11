edd7b4c09a0a584e62e93752d2b31c8e65f5ee7a
Abhijeet Mishra <colouredpages@gmail.com>
2023-06-11 11:40:42 +0530

    excellent with Particles.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-screen">
+        <div className="h-screen bg-gradient">
+          <motion.header
+            className="max-w-6xl flex justify-between items-center mx-auto border-b-2 p-2"
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+          >
+            <motion.h2
+              className="text-3xl text-slate-100 cursor-pointer font-playfair font-semibold tracking-wider 
+          first-letter:font-semibold first-letter:text-4xl underline underline-offset-2"
+            >
+              Abhijeet Mishra
+            </motion.h2>
+            <motion.div className="flex justify-around items-center gap-4">
+              <motion.div className="cursor-pointer text-white">
+                Experience
+              </motion.div>
+              <motion.div className="cursor-pointer text-white">
+                Skills
+              </motion.div>
+              <motion.div className="cursor-pointer text-white">
+                Projects
+              </motion.div>
+              <motion.div className="cursor-pointer text-white">
+                Education
+              </motion.div>
+              <motion.div className="cursor-pointer text-white">
+                Contact Me
+              </motion.div>
+            </motion.div>
+          </motion.header>
+        </div>
+      </div>
+    </>
   );
 }
 
