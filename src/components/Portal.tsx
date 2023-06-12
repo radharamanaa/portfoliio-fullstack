867b962a2d0ef395e9a4eb19b2bd2df7bf9f70ba
Abhijeet Mishra <colouredpages@gmail.com>
2023-06-12 14:30:42 +0530

    modal animation;

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ExperienceData } from "../types/types";
+import { motion } from "framer-motion";
 interface PortalProps {
   exp: ExperienceData;
   closeModal: () => void;
@@ -11,9 +12,14 @@ const Portal: React.FC<PortalProps> = ({ exp, closeModal }) => {
       className="absolute bg-slate-700/50 flex 
     justify-center items-center z-10 h-screen w-full inset-0"
     >
-      <div className="max-w-4xl relative">
+      <motion.div
+        className="max-w-2xl relative"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, type: "spring", stiffness: 80 }}
+      >
         <div
-          className="absolute top-0 right-8 text-black text-2xl"
+          className="absolute top-10 right-8 text-black text-2xl rounded-full bg-slate-600/25 cursor-pointer"
           onClick={() => closeModal()}
         >
           &times;
@@ -26,14 +32,11 @@ const Portal: React.FC<PortalProps> = ({ exp, closeModal }) => {
           <div className="company-name  text-slate-100 self-center text-4xl font-playfair first-letter:text-lime-400">
             {exp.company}
           </div>
-          <div className="duration text-xs text-slate-100 self-center max-w-prose">
+          <div className="duration text-xs text-slate-100 self-center max-w-prose text-justify">
             {exp.aboutCompany}
           </div>
-          <div className="duration text-xs text-slate-100 self-center mb-4">
-            {exp.duration}
-          </div>
-          <div className="role text-sm text-slate-100 pb-1 underline decoration-dashed decoration-1 underline-offset-2 ">
-            {exp.role}
+          <div className="role mt-8 text-sm text-slate-100 pb-1 underline decoration-dashed decoration-1 underline-offset-2 ">
+            {exp.role} - {exp.duration}
           </div>
           <div className="mt-4 achievements text-sm text-slate-100 mb-4 font-raleway tracking-wider indent-8">
             {exp.projects}
@@ -45,7 +48,7 @@ const Portal: React.FC<PortalProps> = ({ exp, closeModal }) => {
             {exp.achievements}
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
