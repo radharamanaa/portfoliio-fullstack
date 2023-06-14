e4b491ec557cb1ba057d66e0e4581f6dfe0a88fb
Abhijeet Mishra <colouredpages@gmail.com>
2023-06-14 09:41:09 +0530

    placeholders for education and contact me done

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const ContactMe: React.FC = () => {
-  return <div>ContactMe</div>;
+  return (
+    <motion.div
+      className="max-w-4xl mx-auto bg-slate-700/50 flex gap-4 flex-wrap 
+    justify-center mb-8 mt-8 text-4xl text-white"
+      initial={{ y: "200vh" }}
+      animate={{ y: 0 }}
+      transition={{ delay: 0.2, type: "spring", stiffness: 40 }}
+    >
+      Contact me - In Progress
+    </motion.div>
+  );
 };
 
 export default ContactMe;
--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Education: React.FC = () => {
-  return <div>Education</div>;
+  return (
+    <motion.div
+      className="max-w-4xl mx-auto bg-slate-700/50 flex gap-4 flex-wrap 
+    justify-center mb-8 mt-8 text-4xl text-white"
+      initial={{ y: "200vh" }}
+      animate={{ y: 0 }}
+      transition={{ delay: 0.2, type: "spring", stiffness: 40 }}
+    >
+      Contact me - In Progress
+    </motion.div>
+  );
 };
 
 export default Education;
