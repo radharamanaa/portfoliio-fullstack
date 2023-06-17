4dcba4eb9cbe8ba466cc8830b31374f10626cf8b
Abhijeet Mishra <colouredpages@gmail.com>
2023-06-17 17:40:05 +0530

    fixes

--- src/components/RightHeader.tsx
+++ src/components/RightHeader.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import baseUrl from "../baseUrl";
 
 const RightHeader: React.FC = () => {
